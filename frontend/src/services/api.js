18868ffbfbd2528237160411e07e5e428c4fd609
omkar <omkarmundhe04@gmail.com>
2026-04-30 23:35:26 +0530

    fixed cors for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://disciplinex-1.onrender.com"
+  baseURL: "https://disciplinex-1.onrender.com/api"
 });
 
 // Attach token automatically
